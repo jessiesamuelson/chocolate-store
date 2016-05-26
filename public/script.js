6fd1b6c0c91a737eb1135c3111f2537129a928bb
Jessie Samuelson <jessie.samuelson@gmail.com>
2016-05-26 03:45:26 -0400

    adds modal functionality

--- public/script.js
+++ public/script.js
@@ -1,38 +1,32 @@
-var data = {
-  "chocolates" : [
-    {
-      "description": "Milk chocolate milder and sweeter because it is made with milk and a higher sugar content than the darker varieties. It also has a smaller quantity of chocolate liquor and, therefore, fewer flavors and aromas.",
-      "id": 1,
-      "type": "milk",
-      "price": 1.50
-    },
-    {
-      "description": "Dark chocolate has the most chocolate liquor and the most intense chocolate flavor. Look for bitter, roasted, fruit, earthy, woodsy and/or nutty notes.",
-      "id": 2,
-      "type": "dark",
-      "price": 2.50
-    },
-    {
-      "description": "White chocolate lacks chocolate liquor, and includes the milk and vanilla used in milk chocolate. These ingredients give it a variety of sweet flavor notes, including cream, milk, honey, vanilla, caramel and/or fruit.",
-      "id": 3,
-      "type": "white",
-      "price": 0.50
-    },
-    {
-      "description": "Treat Grandma to this fine bar of sugar free chocolate",
-      "id": 4,
-      "type": "sugar free",
-      "price": 1
-    }
-  ]
+var table;
+function setData ( ajaxData ) {
+  var data         = ajaxData;
+  var tableElement = document.getElementById('table-body');
+  var totalItems   = document.getElementById("total-items");
+
+  table = new InventoryTable( { chocolates: data.chocolates, table: tableElement, totalItems: totalItems } )
+  table.createTable();
+  return data;
+
 }
 
+$.ajax({
+  url: "/data/inventory.json",
+  success: function ( ajaxData ) {
+    setData(ajaxData);
+  },
+  error: function ( error ) {
+    console.log(error);
+  }
+});
 
+// Define Cart Object
 var Cart = function () {
   this.contents = {};
 }
 
 Cart.prototype = {
+  // Add item to cart
   addToCart: function ( opts ) {
     // If item already exists, increase by one
     // Else add item to cart
@@ -48,17 +42,54 @@ Cart.prototype = {
         price:    price
       }
     }
+  },
+  removeFromCart: function ( type ) {
+    if ( cart.contents[type] ) {
+      this.contents[type].quantity -= 1;
+    }
+    if ( this.contents[type].quantity === 0 ) {
+      delete this.contents[type];
+    }
+    cartModal.populateModal( this.contents );
+  },
+  calculateTotalPrice: function () {
+    var total = 0;
+    for ( var key in this.contents ) {
+      var price = parseFloat(this.contents[key].price);
+      var quantity = this.contents[key].quantity;
+      total += price * quantity;
+    }
+    cartModal.updateTotal(total);
+  },
+  clearCart: function () {
+    this.contents = {};
+    cartModal.populateModal(this.contents);
+    table.updateButton();
+  },
+  calculateTotalItems: function () {
+    var total = 0;
+    for ( var key in this.contents ) {
+      total += this.contents[key].quantity;
+    }
+    return total;
   }
+
 }
 
+// Create cart object
 var cart = new Cart();
 
+// Define InventoryTable object
 var InventoryTable = function ( opts ) {
-  this.chocolates = opts.chocolates;
-  this.table      = opts.table;
+  this.chocolates  = opts.chocolates;
+  this.table       = opts.table;
+  this.totalItems  = opts.totalItems;
 }
 
+// InventoryTable functions
 InventoryTable.prototype = {
+
+  // Create and populate table
   createTable: function () {
     // Loop throught the chocolates array
     for ( var i = 0; i < this.chocolates.length; i++ ) {
@@ -66,93 +97,103 @@ InventoryTable.prototype = {
       this.table.appendChild(entry);
     }
   },
+  // Add row to table
   addTableRow: function ( chocolateObject ) {
+
+    // Row entry element
     var entry = document.createElement('tr');
 
-    var typeDescription = createTableElement({
-      tagName: "td",
-      className: "type-description"
-    });
+    // Entry type and description element
+    var typeDescription = createTableElement({ tagName: "td", className: "type-description" });
     
-    var type = createTableElement({
-      tagName:   "p",
-      className: "type",
-      innerHTML: chocolateObject.type
-    });
+    // Type element
+    var type = createTableElement({ tagName: "p", className: "type", innerHTML: chocolateObject.type });
 
-    var description = createTableElement({
-      tagName:   "p",
-      className: "description",
-      innerHTML: chocolateObject.description
-    });
+    // Description element
+    var description = createTableElement({ tagName: "p", className: "description", innerHTML: chocolateObject.description });
 
+    // Append type/description to typeDescription element
     typeDescription.appendChild(type);
     typeDescription.appendChild(description);
 
-    var price = createTableElement({
-      tagName:   "td",
-      className: "price",
-      innerHTML: chocolateObject.price
-    });
+    // Price element
+    var price = createTableElement({ tagName: "td", className: "price", innerHTML: numberToPrice(chocolateObject.price) });
 
-    var add = createTableElement({
-      tagName:   "td",
-      className: "add"
-    });
+    // Add table element
+    var add = createTableElement({ tagName: "td", className: "add" });
 
-    var addButton = createTableElement({
-      tagName:   "button",
-      className: "add-button",
-      innerHTML: "Add to cart"
-    });
+    // Add button element
+    var addButton = createTableElement({ tagName: "button", className: "add-button", innerHTML: "Add to cart" });
 
+    // Add button data attribute
     addButton.setAttribute("data-button", '{' +
       '"type": "' + chocolateObject.type + '",' +
       '"price": "' + chocolateObject.price + '"' +
       '}');
 
+    // Add button click event
     addButton.onclick = function () {
       var detailObject = JSON.parse(this.getAttribute('data-button'));
       cart.addToCart( detailObject );
-
+      table.updateButton();
     }
 
+    // Append elements to entry
     add.appendChild(addButton);
     entry.appendChild(typeDescription);
     entry.appendChild(price);
     entry.appendChild(add);
     return entry;
   },
+  updateButton: function () {
+    total = cart.calculateTotalItems();
+    this.totalItems.innerHTML = total;
+  }
 }
 
-var chocolates = data.chocolates;
-var table     = document.getElementById('table-body');
-
-var table = new InventoryTable( { chocolates: chocolates, table: table } )
-table.createTable();
-
-
+// Define CartModal
 var CartModal = function ( opts ) {
   this.modal  = opts.modal;
   this.button = opts.button;
-  this.span   = opts.span;
+  // this.span   = opts.span;
   this.table  = opts.table;
+  this.total  = opts.total;
+  this.close  = opts.close;
+  this.clear  = opts.clear;
+
 }
 
-
+// CartModal functions
 CartModal.prototype = {
+  // Open modal
   openModal: function () {
     this.populateModal( cart.contents );
     this.modal.style.display = "block";
   },
+  // Close modal
   closeModal: function () {
     this.modal.style.display = "none";
   },
-  populateModal: function ( cart ) {
-    for ( var key in cart ) {
-      this.addCartRow( { type: key, price: cart[key].price, quantity: cart[key].quantity } )
+  // Populate modal
+  populateModal: function ( cartContents ) {    
+    while (this.table.firstChild) {
+      this.table.removeChild(this.table.firstChild);
     }
+    if ( Object.keys(cartContents).length === 0) {
+      console.log("empty")
+      document.getElementById("empty-message").style.display = "block";
+      document.getElementById("modal-table").style.display = "none";
+    }
+    else {
+      document.getElementById("empty-message").style.display = "none";
+      document.getElementById("modal-table").style.display = "table";
+      for ( var key in cartContents ) {
+        this.addCartRow( { type: key, price: cartContents[key].price, quantity: cartContents[key].quantity } )
+      }
+    }
+    cart.calculateTotalPrice();
   },
+  // Add row to modal table
   addCartRow: function ( cartObject ) {
     var type     = cartObject.type,
         price    = cartObject.price,
@@ -161,58 +202,98 @@ CartModal.prototype = {
     if ( document.getElementById(type + "-entry") ) {
       var entry           = document.getElementById(type + "-entry");
       var quantityElement = entry.children[2];
-      var value = quantityElement.innerHTML;
-      value = Math.floor(value);
-      value++;
-      quantityElement.innerHTML = value;
+      quantityElement.innerHTML = quantity;
     }
     else {
-      // create entry
-      var entry = createTableElement({
-        tagName: "tr",
-        id:      type + "-entry"
-      });
+      // Entry element
+      var entry = createTableElement({ tagName: "tr", id: type + "-entry" });;
 
+      // Type element
+      var typeElement = createTableElement({ tagName: "td", className: "cart-type", innerHTML: type });
 
-      var type = createTableElement({
-        tagName:   "td",
-        className: "cart-type",
-        innerHTML: type
-      })
+      // Price element
+      var priceElement = createTableElement({ tagName: "td", className: "cart-price", innerHTML: numberToPrice(price) });
 
-      var price = createTableElement({
-        tagName:   "td",
-        className: "cart-price",
-        innerHTML: price
-      })
+      // Quantity element
+      var quantityElement = createTableElement({ tagName: "td", className: "cart-quantity", innerHTML: quantity });
 
-      var quantity = createTableElement({
-        tagName:   "td",
-        className: "cart-quantity",
-        innerHTML: quantity
-      })
+      // Remove button element
+      var removeButton = createTableElement({ tagName: "button", className: "cart-remove", innerHTML: "Remove" });
 
-      var removeButton = createTableElement({
-        tagName:   "button",
-        className: "cart-remove",
-        innerHTML: "Remove"
-      })
-
+      // Add click event to remove button
       removeButton.onclick = function () {
-        // TODO: implement remove funtionality
+        cart.removeFromCart( type );
+        table.updateButton();
       }
 
-
-      entry.appendChild(type);
-      entry.appendChild(price);
-      entry.appendChild(quantity);
+      // Add elements to entry
+      entry.appendChild(typeElement);
+      entry.appendChild(priceElement);
+      entry.appendChild(quantityElement);
       entry.appendChild(removeButton);
     }
-
+    // Add entry to table
     this.table.appendChild(entry);
+  },
+  updateTotal: function ( total ) {
+    total = numberToPrice(total)
+    this.total.innerHTML = total;
 
   }
 }
+
+
+// Get the modal
+var modal = document.getElementById('cart-modal');
+
+// Get the button that opens the modal
+var button = document.getElementById("view-cart");
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+var close = document.getElementById("close-modal");
+
+var clear = document.getElementById("clear-modal");
+
+var modalTable = document.getElementById("modal-table-body");
+
+var total = document.getElementById("cart-total");
+
+var cartModal = new CartModal ({
+  modal:  modal,
+  button: button,
+  // span:   span,
+  table:  modalTable,
+  total:  total,
+  close:  close,
+  clear:  clear
+});
+
+// When the user clicks on the button, open the modal 
+cartModal.button.onclick = function() {
+  cartModal.openModal();
+}
+
+// When the user clicks on <span> (x), close the modal
+cartModal.close.onclick = function() {
+  cartModal.closeModal();
+}
+
+cartModal.clear.onclick = function() {
+  cart.clearCart();
+  cartModal.closeModal();
+}
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == cartModal.modal) {
+    cartModal.closeModal();
+  }
+}
+
 
 function createTableElement ( opts ) {
   var tagName   = opts.tagName,
@@ -228,38 +309,19 @@ function createTableElement ( opts ) {
   return element;
 }
 
-// Get the modal
-var modal = document.getElementById('cart-modal');
-
-// Get the button that opens the modal
-var button = document.getElementById("view-cart");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-var modalTable = document.getElementById("modal-table-body")
-
-
-var cartModal = new CartModal ({
-  modal:  modal,
-  button: button,
-  span:   span,
-  table:  modalTable
-});
-
-// When the user clicks on the button, open the modal 
-cartModal.button.onclick = function() {
-  cartModal.openModal();
-}
-
-// When the user clicks on <span> (x), close the modal
-cartModal.span.onclick = function() {
-  cartModal.closeModal();
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == cartModal.modal) {
-    cartModal.closeModal();
+function numberToPrice ( number ) {
+  // number = 0
+  var price;
+  if ( number === 0 ) {
+    price = "0.00";
   }
+  // number has tens digit place
+  else if ( number % 1 > 0 ) {
+    price = number + "0";
+  }
+  // number is integer
+  else {
+    price = number + ".00"
+  }
+  return "$" + price;
 }
